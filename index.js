7e3ea0e48b26c7079365d6417050fbc79d506942
freewind <nowindlee@gmail.com>
2018-09-26 13:43:25 +0800

    modify after dom loaded

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 console.log('document.readyState:', document.readyState);
 
 function listen(label) {
+    document.addEventListener('readystatechange', function (value) {
+        console.log(label, 'readyState -> ', value.target.readyState);
+    });
+
     document.addEventListener('DOMContentLoaded', function (event) {
         console.log(label, 'event: DOMContentLoaded');
         console.log(label, 'document.readyState:', document.readyState);
@@ -15,8 +19,14 @@ function listen(label) {
 listen('');
 
 setTimeout(function () {
+    document.getElementById('main').innerText = 'Hello! (changed)';
+}, 1000);
+
+setTimeout(function () {
     // will never happen
     listen('inSetTimeout:');
 }, 2000);
+
+
 
 

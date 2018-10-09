03101075948a8112f2f37662ce8ac5c0ff4e0410
freewind <nowindlee@gmail.com>
2018-10-09 21:30:24 +0800

    improved

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
-console.log('document.readyState:', document.readyState);
+console.log('initial document.readyState:', document.readyState);
 
 function listen(label) {
-    document.addEventListener('readystatechange', function (value) {
-        console.log(label, 'readyState -> ', value.target.readyState);
-    });
+  document.addEventListener('readystatechange', function (value) {
+    console.log(label, 'event: readystatechange, readyState -> ', value.target.readyState);
+  });
 
-    document.addEventListener('DOMContentLoaded', function (event) {
-        console.log(label, 'event: DOMContentLoaded');
-        console.log(label, 'document.readyState:', document.readyState);
-    });
+  document.addEventListener('DOMContentLoaded', function (event) {
+    console.log(label, 'event: DOMContentLoaded, readyState: ', document.readyState);
+  });
 
-    window.addEventListener('load', function (event) {
-        console.log(label, 'event: load');
-        console.log(label, 'document.readyState:', document.readyState);
-    });
+  window.addEventListener('load', function (event) {
+    console.log(label, 'event: load, readyState: ', document.readyState);
+  });
 }
 
 listen('');
 
 setTimeout(function () {
-    document.getElementById('main').innerText = 'Hello! (changed)';
+  document.getElementById('main').innerText = 'Hello! (changed)';
 }, 1000);
 
 setTimeout(function () {
-    // will never happen
-    listen('inSetTimeout:');
+  // will never happen
+  listen('inSetTimeout:');
 }, 2000);
 
 
